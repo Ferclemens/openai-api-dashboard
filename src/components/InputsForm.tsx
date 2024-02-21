6a311debc7321739323c26b8299e45dcb58d6192
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2024-02-21 13:58:53 -0300

    change env parameters

--- src/components/InputsForm.tsx
+++ src/components/InputsForm.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react"
+import { getResponse } from "../utils/GetApi"
+import { DefaultData } from "../types"
 
-const defaultData = {
+const defaultData: DefaultData = {
     role: '',
     content: ''
 }
 
 export default function Inputs() {
     const [data, setData] = useState(defaultData)
+    //send data
     function onSubmit(e: React.FormEvent<HTMLFormElement>){
         e.preventDefault()
         console.log(data)
+        getResponse(data)
     }
+    //save inputs change
     function onChange(e: React.ChangeEvent<HTMLInputElement>){
         setData((prevState) => ({
             ...prevState,
